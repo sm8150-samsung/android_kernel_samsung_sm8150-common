53ffa9c18bd690c51d1d728027a840c37495eb4c
klabit87 <keith.labit@gmail.com>
2021-02-17 21:33:06 -0600

    OSRC: G973USQU4FTLN

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,7 @@
 
 export ARCH=arm64
 mkdir out
-export SEC_BUILD_CONF_SIGNER_MODEL_NAME=SM-G970U_NA_VZW_USA0
+export SEC_BUILD_CONF_SIGNER_MODEL_NAME=SM-G973U_NA_VZW_USA0
 PIL_SPLITER=$(pwd)/tools/pil_splitter.py
 BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
